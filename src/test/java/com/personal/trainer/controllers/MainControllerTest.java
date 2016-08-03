574e7e60f4dd8fa5596cab3e53cd79c8f01b4a24
Dan <danfoulkes@icloud.com>
2016-08-03 18:48:20 +0100

    adding unit test,

---
+++ src/test/java/com/personal/trainer/controllers/MainControllerTest.java
@@ -0,0 +1,16 @@
+package com.personal.trainer.controllers;
+
+import org.junit.Test;
+
+/**
+ * Created by danfoulkes on 03/08/2016.
+ */
+public class MainControllerTest {
+
+
+    @Test
+    public void testMailBody(){
+
+    }
+
+}
